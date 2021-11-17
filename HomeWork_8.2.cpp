0ffd9952d10b0f370e92e1a47e3ad3fdec470741
HoTToDoug <hotto.doug@mail.ru>
2021-11-17 16:46:54 +0300

    Add file

---
+++ HomeWork_8.2.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include "DZ8.h"
+
+using namespace std;
+
+
+
+int main()
+{
+	
+
+	{
+		
+		cout << "Task 1:\n";
+		int numerator = 10;
+		double denominator = 18;
+		try
+		{
+			if (denominator == 0) throw overflow_error("Division by zero");
+			cout << numerator << " / " << denominator << " = " << div(numerator, denominator);
+		}
+		catch (overflow_error)
+		{
+			cout << "Division by zero!";
+		}
+	}
+
+	
+
+	{
+		
+		cout << "\n\nTask 2:\n";
+		Bar* bar = new Bar();
+		int n = 0;
+		try
+		{
+			bool nNotZero = true;
+			while (nNotZero)
+			{
+				cout << "Enter n: ";
+				cin >> n;
+				if (n != 0)
+				{
+					bar->set(n);
+				}
+				else nNotZero = false;
+			}
+		}
+		catch (Ex& e)
+		{
+			cout << "Exception thrown, code " << e.getX();
+		}
+		delete bar;
+	}
+
+	
+
+	{
+		
+		cout << "\nTask 3:\n";
+		int fieldSize = 10;
+		Robot* robot = new Robot(fieldSize);
+
+		while (true)
+		{
+			robot->printCoordinates();
+			robot->move();
+		}
+		delete robot;
+		
+	}
+
+	
+
+}
